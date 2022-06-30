eae1900e434fcf98fc73c7526079d86e60ff616c
jjavier-bm <jjbm.research@gmail.com>
2022-06-30 18:12:23 +0100

    alternative plot fix16

--- pisacov/core/contacts.py
+++ pisacov/core/contacts.py
@@ -547,4 +547,4 @@ class contact_atlas:
 
         if plot_type == 'png' or plot_type == 'eps':
             fig.savefig(outpath, format=plot_type, overwrite=True)
-            plt.close(fig.fig)
+            plt.close(fig)
