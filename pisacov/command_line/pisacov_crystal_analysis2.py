d0e477d42d2d57ee806e137e2af21b5ec5b10c75
jjavier-bm <jjbm.research@gmail.com>
2023-03-30 14:58:51 +0100

    create test script

--- pisacov/command_line/pisacov_crystal_analysis2.py
+++ pisacov/command_line/pisacov_crystal_analysis2.py
@@ -9,9 +9,11 @@ __script__ = 'PISACov Crystal Analysis script'
 
 from pisacov import command_line as pcl
 
+from pisacov import iomod as pio
 from pisacov.iomod import paths as ppaths
 from pisacov.iomod import _conf_ops as pco
 from pisacov.iomod import outcsv as pic
+from pisacov.iomod import plots as pip
 from pisacov.sys import crop as psc
 from pisacov.sys import dmp as psd
 from pisacov.sys import msagen as psm
@@ -22,7 +24,9 @@ from pisacov.core import interfaces as pci
 
 from crops.iomod import parsers as cps
 
-import numpy as np
+from conkit import io as ckio
+
+# import numpy as np
 import argparse
 import os
 import datetime
@@ -39,17 +43,16 @@ def create_argument_parser():
     parser.add_argument("seqpath", nargs=1, metavar=("Seqfile"),
                         help="Input original sequence filepath.")
     parser.add_argument("crystalpath", nargs=1,
-                         metavar=("Crystal_structure"),
-                         help="Input original crystal structure filepath.")
+                        metavar=("Crystal_structure"),
+                        help="Input original crystal structure filepath.")
 
     # Use CROPS
-    parser.add_argument("-r", "--remove_insertions", action='store_true', # DO IT FOR SIFTS AND ALSO OPTIONALLY FOR CUSTOM CSV
-                             default=False,
-                             help="Use CROPS and SIFTS database to remove insertions from crystal structure.")
+    parser.add_argument("-r", "--remove_insertions", action='store_true',  # DO IT FOR SIFTS AND ALSO OPTIONALLY FOR CUSTOM CSV
+                        default=False,
+                        help="Use CROPS and SIFTS database to remove insertions from crystal structure.")
     parser.add_argument("-u", "--uniprot_threshold", nargs=1,
-                            metavar=("Uniprot_ratio_threshold"),
-                            help='If Uniprot sequence contribution to crystal sequence is below threshold ( %% ), all its residues are removed. [MIN,MAX)=[0,100).')
-
+                        metavar=("Uniprot_ratio_threshold"),
+                        help='If Uniprot sequence contribution to crystal sequence is below threshold ( %% ), all its residues are removed. [MIN,MAX)=[0,100).')
 
     # HHBLITS modification
     parser.add_argument("-a", "--hhblits_arguments", nargs=5,
@@ -59,6 +62,11 @@ def create_argument_parser():
                         help=("Introduce HHBLITS arguments." + os.linesep +
                               "DeepMetaPSICOV & PISACOV DEFAULT: [3, 0.001, 'inf', 50, 99]. " +
                               os.linesep + "HHBlits DEEFAULT: [2, 0.001, 1000, 0, 90]"))
+
+    # THRESHOLD ABOVE WHICH CONTACTS COUNT
+    parser.add_argument("-l", "--lower_threshold", nargs=3,
+                        metavar=("psicov", "ccmpred", "dmp"),
+                        help=("Remove predicted contacts scored below these values. Use '-inf' for no lower cutoff."))
 
     # SKIP CONPRED
     parser.add_argument("-s", "--skip_conpred", action='store_true',
@@ -71,6 +79,9 @@ def create_argument_parser():
     parser.add_argument("-c", "--collection_file", nargs=1,
                         metavar=("Collection_CSV_Path"),
                         help="Path to CSV file where pisacov signals will be appended. Default: outdir/evcovsignal.cropped.pisacov.csv and outdir/evcovsignal.full.pisacov.csv.")
+    parser.add_argument("-p", "--plot_formats", nargs='+',
+                        metavar=("Plot file format(s)"),
+                        help="One or more formats of 'png', 'eps', 'svg' and 'agr' of figures/data to be produced.")
 
     parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
 
@@ -102,6 +113,11 @@ def main():
 
     if args.hhblits_arguments is not None:
         invals['HHBLITS_PARAMETERS'] = pco._check_hhparams(args.hhblits_arguments)
+    else:
+        pass
+
+    if args.lower_threshold is not None:
+        invals['LOWTHRESHOLD'] = pco._check_lowth(args.lower_threshold)
     else:
         pass
 
@@ -153,15 +169,28 @@ def main():
             invals['OUTCSVPATH'].append(None)
             invals['OUTCSVPATH'].append(ppaths.check_path(args.collection_file[0]))
 
+    if args.plot_formats is None:
+        plotformats={'png'}
+    else:
+        plotformats=set()
+        for element in args.plot_formats:
+            if element.lower() in {'png', 'eps', 'svg', 'agr'}:
+                plotformats.add(element.lower())
+            else:
+                msg = element + " is not a valid plot format. Ignored."
+                logger.warning(msg)
+
     # Define formats used
-    sources = pco._sources()
+    sources = pco._sources(lowth=invals['LOWTHRESHOLD'])
 
     # Parse sequence and structure files
     logger.info('Parsing sequence file...')
-    seqs = cps.parseseqfile(invals['INSEQ'])
+    # seqs = cps.parseseqfile(invals['INSEQ'])
+    seqs = pio.read(invals['INSEQ'], 'fasta')
 
     logger.info('Parsing structure file...')
-    strs, filestrs = cps.parsestrfile(invals['INSTR'])
+    # strs, filestrs = cps.parsestrfile(invals['INSTR'])
+    strs, filestrs = pio.read(invals['INSTR'], 'pdb')
 
     if len(seqs) == 1 or len(strs) == 1:
         if len(seqs) == 1:
@@ -212,8 +241,6 @@ def main():
             logger.info(pcl.running('CROPS-renumber', done=itime))
 
         ppaths.mdir(outpdbdir)
-        if cropping is False:
-            psc.splitseqs(invals['INSEQ'], outpdbdir)
         copyfile(invals['INSTR'], instrc)
 
     for i, iseq in seq.imer.items():
@@ -221,30 +248,9 @@ def main():
         fseq[i] = os.path.join(invals['OUTROOT'], pdbid, fiseq)
         fiseq = pdbid + '_' + i + '.msa.aln'
         fmsa[i] = os.path.join(invals['OUTROOT'], pdbid, 'hhblits', fiseq)
-        if skipexec is False:
-            iseq.dump(fseq[i])
+        # if skipexec is False:
+        #    iseq.dump(fseq[i])
 
-    # Parse cropped sequences and maps
-    if cropping is True:
-        amap = {}
-        fcropseq = {}
-        fcropmsa = {}
-        for i, iseq in seq.imer.items():
-            fprefix = pdbid + '_' + i + '.crops.to_uniprot'
-            fmap = os.path.join(invals['OUTROOT'], pdbid,
-                                fprefix + os.extsep + 'cropmap')
-            amap.update(cps.parsemapfile(fmap)[pdbid])
-            fcropseq[i] = os.path.join(invals['OUTROOT'], pdbid,
-                                       fprefix + os.extsep + 'fasta')
-            fcropmsa[i] = os.path.join(invals['OUTROOT'], pdbid, 'hhblits',
-                                       (fprefix + os.extsep +
-                                        'msa' + os.extsep + 'aln'))
-            seq.set_cropmaps(amap, cropmain=True)
-
-    # EXECUTION OF EXTERNAL PROGRAMS
-    hhdir = os.path.join(invals['OUTROOT'], pdbid, 'hhblits', '')
-    dmpdir = os.path.join(invals['OUTROOT'], pdbid, 'dmp', '')
-    pisadir = os.path.join(invals['OUTROOT'], pdbid, 'pisa', '')
     fstr = os.path.join(invals['OUTROOT'], (pdbid + os.extsep + 'crops' +
                                             os.extsep + 'seq' +
                                             os.extsep + 'pdb'))
@@ -254,68 +260,72 @@ def main():
                                  os.extsep + 'oldids' +
                                  os.extsep + 'to_uniprot' +
                                  os.path.splitext(invals['INSTR'])[1]))
-    if skipexec is False:
-        # MSA GENERATOR
-        ppaths.mdir(hhdir)
-        if invals['HHBLITS_PARAMETERS'] == ['3', '0.001', 'inf', '50', '99']:
-            logger.info('Generating Multiple Sequence Alignment using DeepMetaPSICOV default parameters... [AS RECOMMENDED]')
-        elif invals['HHBLITS_PARAMETERS'] == ['2', '0.001', '1000', '0', '90']:
-            logger.info('Generating Multiple Sequence Alignment using HHBlits default parameters...')
-        else:
-            logger.info('Generating Multiple Sequence Alignment using user-custom parameters...')
 
-        for i, iseq in seq.imer.items():
-            sfile = fcropseq[i] if cropping is True else fseq[i]
-            afile = fcropmsa[i] if cropping is True else fmsa[i]
-            logger.info(pcl.running('HHBlits'))
-            itime = datetime.datetime.now()
-            themsa = psm.runhhblits(sfile,
-                                    invals['HHBLITS_PARAMETERS'],
-                                    hhdir)
-            logger.info(pcl.running('HHBlits', done=itime))
-            if cropping is True:
-                iseq.cropmsa = themsa
-                if iseq.ncrops() == 0:
-                    iseq.msa = iseq.cropmsa
-                    logger.info('    Cropped sequence ' +
-                                iseq.oligomer_id + '_' + iseq.name +
-                                ' is identical to original sequence.')
-                    continue
-                else:
-                    pass
-            else:
-                iseq.msa = themsa
-
-    # DEEP META PSICOV RUN
-    ppaths.mdir(dmpdir)
-    if skipexec is False:
-        logger.info('Generating contact prediction lists via DeepMetaPSICOV...')
-        for i, iseq in seq.imer.items():
-            sfile = fcropseq[i] if cropping is True else fseq[i]
-            afile = fcropmsa[i] if cropping is True else fmsa[i]
-            nsfile = os.path.join(dmpdir, os.path.basename(sfile))
-            if sfile != nsfile:
-                copyfile(sfile, nsfile)
-            logger.info(pcl.running('DeepMetaPSICOV'))
-            itime = datetime.datetime.now()
-            psd.rundmp(nsfile, afile, dmpdir)
-            logger.info(pcl.running('DeepMetaPSICOV', done=itime))
-
-    # INTERFACE GENERATION, PISA
+    # EXECUTION OF PISA, INTERFACE FILES GENERATION AND PARSING
+    pisadir = os.path.join(invals['OUTROOT'], pdbid, 'pisa', '')
     ppaths.mdir(pisadir)
+    sfile = fcropstr if cropping is True else fstr
     if skipexec is False:
         logger.info('Generating interface files via PISA...')
-        sfile = fcropstr if cropping is True else fstr
         logger.info(pcl.running('PISA'))
         itime = datetime.datetime.now()
         iflist = psp.runpisa(sfile, pisadir, sessionid = pdbid)
         logger.info(pcl.running('PISA', done=itime))
+    else:
+        ixml = os.path.join(pisadir,
+                            (os.path.splitext(os.path.basename(sfile))[0] +
+                             os.extsep + 'interface' +
+                             os.extsep + 'xml'))
+        axml = os.path.join(pisadir,
+                            (os.path.splitext(os.path.basename(sfile))[0] +
+                             os.extsep + 'assembly' +
+                             os.extsep + 'xml'))
+
+        iflist = pci.parse_interface_xml(ixml, axml)
+
+    for i in range(len(iflist)):
+        iflist[i].chains[0].seq_id = seq.whatseq(iflist[i].chains[0].crystal_id)
+        iflist[i].chains[1].seq_id = seq.whatseq(iflist[i].chains[1].crystal_id)
+        if iflist[i].chains[0].type == 'Protein':
+            seq.imer[iflist[i].chains[0].seq_id].biotype = 'Protein'
+        if iflist[i].chains[1].type == 'Protein':
+            seq.imer[iflist[i].chains[1].seq_id].biotype = 'Protein'
+
+    # Parse cropped sequences and maps
+    if cropping is True:
+        amap = {}
+        fcropseq = {}
+        fcropmsa = {}
+        for i, iseq in seq.imer.items():
+            if iseq.biotype == 'Protein':
+                fprefix = pdbid + '_' + i + '.crops.to_uniprot'
+                fmap = os.path.join(invals['OUTROOT'], pdbid,
+                                    fprefix + os.extsep + 'cropmap')
+                amap.update(cps.parsemapfile(fmap)[pdbid])
+                fcropseq[i] = os.path.join(invals['OUTROOT'], pdbid,
+                                           fprefix + os.extsep + 'fasta')
+                fcropmsa[i] = os.path.join(invals['OUTROOT'], pdbid, 'hhblits',
+                                           (fprefix + os.extsep +
+                                            'msa' + os.extsep + 'aln'))
+                seq.set_cropmaps(amap, cropmain=True)
+                if iseq.ncrops() == 0:
+                    logger.info('    Cropped sequence ' +
+                                iseq.oligomer_id + '_' + iseq.name +
+                                ' is identical to the original sequence.')
+                else:
+                    logger.info('    Cropped sequence ' +
+                                iseq.oligomer_id + '_' + iseq.name +
+                                ' is ' + str(iseq.ncrops()) + ' residues ' +
+                                'shorter than the original sequence.')
+            else:
+                logger.info('    Cropped sequence ' +
+                            iseq.oligomer_id + '_' + iseq.name +
+                            ' is not a Protein chain. Ignoring.')
 
     endmsg = pcl.ok(starttime, command=__script__)
     logger.info(endmsg)
 
     return
-
 
 if __name__ == "__main__":
     import sys
@@ -323,7 +333,6 @@ if __name__ == "__main__":
 
     try:
         main()
-        logger.info(pcl.ok())
         sys.exit(0)
     except Exception as e:
         if not isinstance(e, SystemExit):
