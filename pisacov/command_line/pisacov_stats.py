2c40cda3cf1e0957e74dc12826abd5a67bba478d
jjavier-bm <jjbm.research@gmail.com>
2023-06-22 20:12:51 +0100

    minor bezier bug 9

--- pisacov/command_line/pisacov_stats.py
+++ pisacov/command_line/pisacov_stats.py
@@ -341,7 +341,7 @@ def main():
                                          area_for_color=areas_dict_best[name],
                                          plot_type=imtype,
                                          roc_type='probvsscore',
-                                         scores=([wholescores[name[:-3]][0]]+wholescores[name]))
+                                         scores=([wholescores[name[:-3]][0]]+wholescores[name[:-3]]))
         # TOC CURVES
         #for name in names2:
         #    fout = os.path.join(pdir, fname + '.' + name + '.toc.' + imtype)
