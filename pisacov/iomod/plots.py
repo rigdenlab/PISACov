404bed1a82d7acdc5e1b92bcf7ddd468ba687021
jjavier-bm <jjbm.research@gmail.com>
2023-12-09 15:50:14 +0000

    comment out plots.py extra code

--- pisacov/iomod/plots.py
+++ pisacov/iomod/plots.py
@@ -751,46 +751,46 @@ def plot_area_histogram(data, outpath, plot_type='png'):
         plt.close(fig)
 
 ####################################################################################################
-"""
-=====================
-3D surface (colormap)
-=====================
+#"""
+#=====================
+#3D surface (colormap)
+#=====================
 
-Demonstrates plotting a 3D surface colored with the coolwarm colormap.
-The surface is made opaque by using ``antialiased=False``.
+#Demonstrates plotting a 3D surface colored with the coolwarm colormap.
+#The surface is made opaque by using ``antialiased=False``.
 
-Also demonstrates using the `.LinearLocator` and custom formatting for the
-z axis tick labels.
-"""
+#Also demonstrates using the `.LinearLocator` and custom formatting for the
+#z axis tick labels.
+#"""
 
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
-import numpy as np
+#import matplotlib.pyplot as plt
+#from matplotlib import cm
+#from matplotlib.ticker import LinearLocator
+#import numpy as np
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+#fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
 # Make data.
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
+#X = np.arange(-5, 5, 0.25)
+#Y = np.arange(-5, 5, 0.25)
+#X, Y = np.meshgrid(X, Y)
+#R = np.sqrt(X**2 + Y**2)
+#Z = np.sin(R)
 
 # Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+#surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+#                       linewidth=0, antialiased=False)
 
 # Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
-ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.set_zlim(-1.01, 1.01)
+#ax.zaxis.set_major_locator(LinearLocator(10))
 # A StrMethodFormatter is used automatically
-ax.zaxis.set_major_formatter('{x:.02f}')
+#ax.zaxis.set_major_formatter('{x:.02f}')
 
 # Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+#fig.colorbar(surf, shrink=0.5, aspect=5)
 
-plt.show()
+#plt.show()
 
 
 #############################################################################
